9e03ae8be8e27ee6bd6443f90e346cff20acb6bb
Morad Irshaid <151387049+moradirsh@users.noreply.github.com>
2026-01-04 15:55:43 -0800

    Solved uvicorn issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from app.api import case_router
+from app.core.database import Base, engine
 
-# attempting to solve issue with circular imports
-app = FastAPI(title="Workflow Management Platform")
+app = FastAPI(title = "Workflow Management Platform")
 
+#test endpoint
 @app.get("/")
 def root():
-    return {"message": "API is running"}
+    return {"message": "Welcome to the Workflow Management Platform API"}
+
+# Include the case API endpoints
+app.include_router(case_router.router)
+
+# Create tables in Postgres automatically
+Base.metadata.create_all(bind=engine)
